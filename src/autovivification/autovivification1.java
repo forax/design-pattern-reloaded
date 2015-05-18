05f30f3e06c8c96c0d569ffa70a2a1e11af9cb3c
Remi Forax <forax@univ-mlv.fr>
2015-05-18 15:23:13 +0200

    Add auto-vivification

---
+++ src/autovivification/autovivification1.java
@@ -0,0 +1,25 @@
+package autovivification;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public interface autovivification1 {
+  interface Dict<K,V> {
+    V get(K key);
+    
+    public static <K,V> Dict<K, V> asDict(Map<K, V> map, Supplier<? extends V> factory) {
+      return key -> map.computeIfAbsent(key, k -> factory.get());
+    }
+  }
+
+  public static void main(String[] args) {
+    HashMap<String, List<String>> map = new HashMap<>();
+    Dict<String, List<String>> dict = Dict.asDict(map, ArrayList::new);
+    dict.get("foo").add("bar");
+    dict.get("foo").add("baz");
+    System.out.println(map);
+  }
+}
