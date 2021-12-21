4bd865387181bc16b7c4cd4ca5796437ba1c2deb
forax <forax@univ-mlv.fr>
2021-12-21 21:41:41 +0100

    provide a better implementation of the generic builder

--- src/main/java/builder/builder2.java
+++ src/main/java/builder/builder2.java
@@ -3,12 +3,15 @@ package builder;
 import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.invoke.MethodType;
 import java.lang.invoke.SerializedLambda;
 import java.lang.reflect.RecordComponent;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Map;
 
 import static java.lang.invoke.MethodType.methodType;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.IntStream.range;
 
 public interface builder2 {
   record Spaceship(String name, String captain, int torpedoes, int length) {}
@@ -23,13 +26,33 @@ public interface builder2 {
       throw (T) cause;
     }
 
+    private record HiddenClass(MethodType constructorType, Map<String, Integer> slotMap) { }
+
+    private static final ClassValue<HiddenClass> HIDDEN_CLASS_VALUE =
+        new ClassValue<>() {
+          @Override
+          protected HiddenClass computeValue(Class<?> type) {
+            var components = type.getRecordComponents();
+            var slotMap = range(0, components.length)
+                .boxed()
+                .collect(toMap(i -> components[i].getName(), i -> i));
+            var  constructorType= methodType(void.class,
+                Arrays.stream(components).map(RecordComponent::getType).toArray(Class[]::new));
+            return new HiddenClass(constructorType, slotMap);
+          }
+        };
+
     private final Lookup lookup;
     private final Class<T> type;
-    private final HashMap<String, Object> map = new HashMap<>();
+    private final HiddenClass hiddenClass;
+    private final Object[] values;
 
     public Builder(Lookup lookup, Class<T> type) {
       this.lookup = lookup;
       this.type = type;
+      var hiddenClass = HIDDEN_CLASS_VALUE.get(type);
+      this.hiddenClass = hiddenClass;
+      values = new Object[hiddenClass.slotMap.size()];
     }
 
     public <K> Builder<T> with(Accessor<T, K> accessor, K value) {
@@ -42,21 +65,18 @@ public interface builder2 {
       } catch (Throwable e) {
         throw rethrow(e);
       }
-      map.put(lambda.getImplMethodName(), value);
+      var slot = hiddenClass.slotMap.get(lambda.getImplMethodName());
+      if (slot == null) {
+        throw new IllegalArgumentException("invalid method reference " + accessor);
+      }
+      values[slot] = value;
       return this;
     }
     
     public T build() {
-      var components = type.getRecordComponents();
-      var array = new Object[components.length];
-      for (var i = 0; i < components.length; i++) {
-        var component = components[i];
-        array[i] = map.computeIfAbsent(component.getName(), k -> { throw new IllegalStateException("no value '" + k + "' defined"); });
-      }
       try {
-        var constructor = lookup.findConstructor(type,
-            methodType(void.class, Arrays.stream(components).map(RecordComponent::getType).toArray(Class[]::new)));
-        return type.cast(constructor.invokeWithArguments(array));
+        var constructor = lookup.findConstructor(type, hiddenClass.constructorType);
+        return type.cast(constructor.invokeWithArguments(values));
       } catch (NoSuchMethodException | InstantiationException | IllegalAccessException e) {
         throw (LinkageError) new LinkageError().initCause(e);
       } catch(Throwable e) {
